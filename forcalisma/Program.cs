2ad4cde1bd8ccaca7fd5c892a936c9737d1947ca
Ercanincee <132362073+Ercanincee@users.noreply.github.com>
2023-11-07 21:50:25 +0300

    Add project files.

---
+++ forcalisma/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i < 101; i++)
+{
+    if(i%3==0)
+    {
+        Console.WriteLine(i.ToString()+"Fizz");
+    }
+    if (i % 5 == 0)
+    {
+        Console.WriteLine(i.ToString() + "Buzz");
+    }
+    if(i%5==0&&i%3==0) { Console.WriteLine(i.ToString() + "Fizz" + "Buzz"); }
+}
